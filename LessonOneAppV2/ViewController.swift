e42c9fefee65330e8033e05c0df6e5307c7166c6
Mikhail <bskfrost3@yandex.ru>
2020-10-15 12:30:26 +0700

    Initial Commit

---
+++ LessonOneAppV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonOneAppV2
+//
+//  Created by mac on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
